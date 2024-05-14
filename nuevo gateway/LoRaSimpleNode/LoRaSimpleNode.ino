f947b93ecf86f4c8a63c64468f9319ad0a4896c8
Javier <al2153035335@azc.uam.mx>
2024-05-14 00:19:10 -0600

    rtc y json

--- nuevo gateway/LoRaSimpleNode/LoRaSimpleNode.ino
+++ nuevo gateway/LoRaSimpleNode/LoRaSimpleNode.ino
@@ -1,6 +1,8 @@
 #include <SPI.h>              // include libraries
 #include <LoRa.h>
 #include <Wire.h>
+#include <ESP32Time.h>
+#include <ArduinoJson.h>
 
 #define SCK     5    // GPIO5  -- SCK
 #define MISO    19   // GPIO19 -- MISO
@@ -12,15 +14,37 @@
 
 // String rssi = "RSSI --";
 // String packSize = "--";
-String id = "1";
-
+int id = 1;
+int period=1;
 String packet ;
 
 struct NodeInfo{
-  String id;
-  String value;
+  int id;
+  float value; 
 };
 struct NodeInfo node;
+
+ESP32Time rtc(-21600);
+
+// void LoRa_onReceive(void *parameter) {
+//   TickType_t xLastWakeTime = xTaskGetTickCount();
+//   const TickType_t xDelay = pdMS_TO_TICKS(100); // Esperar 1 segundo
+//   while (true) {
+//     LoRa_rxMode();
+//     int packetSize = LoRa.parsePacket();
+//     if (packetSize) { 
+//       readMessage(packetSize);
+//       Serial.println(packet);
+//       // if (packet == node.id) {
+//       //   LoRa_sendMessage((String)value);
+
+//       // }
+//       // LoRa_rxMode();
+//     }
+//     vTaskDelayUntil(&xLastWakeTime, xDelay);
+//     // Dormir durante un breve período para evitar sobrecargar el procesador
+//   }
+// }
 
 
 void setup() {
@@ -37,7 +61,15 @@ void setup() {
   LoRa.setSpreadingFactor(7);
   LoRa.setCodingRate4(7);
   LoRa.enableCrc();
-
+  // xTaskCreatePinnedToCore(
+  //   LoRa_onReceive,            // Función de la tarea
+  //   "LoRaTask",                // Nombre de la tarea
+  //   4096,                      // Tamaño del stack de la tarea
+  //   NULL,                      // Parámetro de entrada de la tarea
+  //   3,                         // Prioridad de la tarea (0 es la más baja)
+  //   NULL,                      // Manejador de la tarea
+  //   0                          // Núcleo en el que se ejecutará la tarea (0 o 1)
+  // );
   Serial.println("LoRa init succeeded.");
   Serial.println();
   Serial.println("LoRa Simple Node");
@@ -53,25 +85,61 @@ void setup() {
   Serial.println(node.id);
 }
 
-int count=0;
 
 void loop() {
-
   int packetSize = LoRa.parsePacket();
-  if (packetSize) { 
-    readMessage(packetSize);
-    Serial.println(packet);
-    // unsigned long lastTime=millis();
-    // while((millis()-lastTime)<70);
-    if (packet==(String)node.id){
-      LoRa_sendMessage((String)count);
-      LoRa_rxMode();
-      count++;
+    if (packetSize) { 
+      readMessage(packetSize);
+      Serial.println(packet);
+      // String input;
+
+      JsonDocument doc;
+
+      DeserializationError error = deserializeJson(doc, packet);
+
+      if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        // return;
+      }
+      else{
+        const char* command = doc["command"]; // "setTime"
+        long arg = doc["arg"]; // 1351824120
+        int id = doc["nodeId"];
+        if (id==node.id){
+          if (String(command)=="setTime"){
+            rtc.setTime(arg); 
+            Serial.println(String(rtc.getLocalEpoch()));
+          }
+          else if(String(command)=="setPeriod"){
+
+          }
+          else if (String(command)=="getValues"){
+
+          }
+          else if(String(command)=="getInfo"){
+
+          }
+          else{
+
+          }
+
+        }
+        
+       
+      }
+
+      
+
+      // Serial.println(packet.substring(0, packet.indexOf(":")));
+      // Serial.println(packet.substring(packet.indexOf(":")+1));
+      // if (packet == node.id) {
+      //   LoRa_sendMessage((String)value);
+
+      // }
+      // LoRa_rxMode();
     }
-    
-  }
-  
-  
+  delay(10);
 }
 
 void LoRa_rxMode(){
